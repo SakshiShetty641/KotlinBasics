fd6173484b847200439c26c0917090df06be743f
sakshishetty1231997@gmail.com <SakshiShetty641>
2024-06-17 10:53:12 +0530

    Variables and data types

---
+++ src/NullableClass.kt
@@ -0,0 +1,8 @@
+fun main() {
+    /*
+   Kotlin has nullable types, which means a variable can hold either a value or null.
+   To declare a nullable variable, you use a question mark (?) after the type
+     */
+ var str: String? = null
+
+}
---
+++ src/Variables.kt
@@ -0,0 +1,13 @@
+fun main() {
+    /*
+    var and val is used to define variables
+    val is used as a final keyword, so once assigned it cannot be changed
+     */
+
+    val str = "Hello World"
+    println(str)
+
+    var variable = "Momday"
+    println(variable)
+
+}
